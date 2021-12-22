5991ffe6576392af69d78e5b3927384d4fa8c370
reffaeli <cremboo@gmail.com>
2021-12-22 09:22:03 +0200

    Add files via upload

---
+++ assets/js/floatbot.js
@@ -0,0 +1,14 @@
+
+!function(t,e,o,c,n,a){var s=window.nanorep=window.nanorep||{};s=s[e]=s[e]||{},s.apiHost=a,s.host=n,s.path=c,s.account=t,s.protocol="https:",s.on=s.on||function(){s._calls=s._calls||[],s._calls.push([].slice.call(arguments))};var p=s.protocol+"//"+n+c+o+"?account="+t,l=document.createElement("script");l.async=l.defer=!0,l.setAttribute("src",p),document.getElementsByTagName("head")[0].appendChild(l)}("demosupport3","floatingWidget","floating-widget.js","/web/","demosupport3.nanorep.co");
+
+
+nanorep.floatingWidget.on({
+	init: function() {
+		this.setConfigId('955231562');
+        this.setKB('955230942');
+
+	}
+});
+
+
+
